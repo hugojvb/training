d1d54ace79e3a75abe65d818edac1667832c4eda
hugojvb <hugojvb20@gmail.com>
2021-10-04 14:51:13 +0100

    Removed bloat files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TestService } from './services/testqw/test.service';
-import { AnotherService } from './services/another/another.service';
-import { TestControllerController } from './controllers/test-controller/test-controller.controller';
-import { AnotherTestControllerController } from './controllers/another-test-controller/another-test-controller.controller';
 
 @Module({
   imports: [],
-  controllers: [
-    AppController,
-    TestControllerController,
-    AnotherTestControllerController,
-  ],
-  providers: [AppService, TestService, AnotherService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- backend/src/controllers/another-test-controller/another-test-controller.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('another-test-controller')
-export class AnotherTestControllerController {}
