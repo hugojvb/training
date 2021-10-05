3da1aef08595520995e0f9380cc7fce582bd8c60
hugojvb <hugojvb20@gmail.com>
2021-10-05 22:25:49 +0100

    Added static files load in nestjs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<h1>Ready to go!</h1>
+		</div>
+	);
 }
 
 export default App;
