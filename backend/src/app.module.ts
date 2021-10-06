1b0c37b242ec72e88208b919a1bd40837927f8df
hugojvb <hugojvb20@gmail.com>
2021-10-06 21:21:39 +0100

    Added mongoose for odm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,14 @@ import { UsersService } from './services/users/users.service';
 import { UsersController } from './controllers/users/users.controller';
 import { join } from 'path';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '../../frontend', 'build'),
     }),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
   ],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
